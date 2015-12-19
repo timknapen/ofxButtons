6cb7df3cd585f41ee9fbded4ebada080611a9fc5
Tim <timknapen@gmail.com>
2015-12-19 15:20:34 +0100

    fixed version bug

--- src/ofxButtons.h
+++ src/ofxButtons.h
@@ -9,6 +9,13 @@
 #define BUTTONS_COLOR_MEDIUM        0x77dd77        //0x66cc66
 #define BUTTONS_COLOR_LIGHT         0xdddddd
 
+// Fix OF version differences
+#if OF_VERSION_MAJOR <= 0 && OF_VERSION_MINOR <= 8
+#define		ofDrawRectangle(a,b,c,d)	ofRect(a,b,c,d)
+#define		ofDrawEllipse(a,b,c,d)		ofEllipse(a,b,c,d)
+#define		ofDrawLine(a,b,c,d)			ofLine(a,b,c,d)
+#endif
+
 #include "ofMain.h"
 #include "SimpleButton.h"
 
